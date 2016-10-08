63e2dfb3802b728109f8655e89407b1b086fcf3f
Nikita <nikitafart@mail.ru>
2016-10-08 23:09:49 +0400

    finished  xmlTask and fixes

--- starting-monkey-to-human-path/src/RPIS41/Fartushnov/wdad/Application.java
+++ starting-monkey-to-human-path/src/RPIS41/Fartushnov/wdad/Application.java
@@ -1,8 +1,5 @@
 package RPIS41.Fartushnov.wdad;/**
- * Created by никита on 01.10.2016.
+ * Created by никита on 07.10.2016.
  */
 public class Application {
-    public static void main(String[] args){
-        System.out.println("I'm Nikita Fartushnov, and I'm not a monkey");
-    }
 }
---
+++ starting-monkey-to-human-path/src/RPIS41/Fartushnov/wdad/learn/xml/TestXmlTask.java
@@ -0,0 +1,15 @@
+package RPIS41.Fartushnov.wdad.learn.xml;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by никита on 07.10.2016.
+ */
+public class TestXmlTask {
+    public static void main(String[] args) {
+
+    }
+
+}
