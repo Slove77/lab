039bb4eb6f237ccb37da2094384baf5861c2fad5
Nikita <nikitafart@mail.ru>
2016-10-08 23:18:50 +0400

    complete testXmlTask

--- starting-monkey-to-human-path/src/RPIS41/Fartushnov/wdad/learn/xml/TestXmlTask.java
+++ starting-monkey-to-human-path/src/RPIS41/Fartushnov/wdad/learn/xml/TestXmlTask.java
@@ -9,7 +9,16 @@ import java.util.Calendar;
  */
 public class TestXmlTask {
     public static void main(String[] args) {
-
+        XmlTask test = new XmlTask();
+        try{
+            test.changeOfficiantName("garik","sidorov","vova","petrov"); //смена имени официанта
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            calendar.setTime(sdf.parse("7-5-2016"));
+            //test.removeDay(calendar);
+            System.out.println(test.earningsTotal("petrov",calendar));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
-
 }
